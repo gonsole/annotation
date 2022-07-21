950bb480e2fec21bd9419c5c7fb4e1a7385cc6f8
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-07-21 23:52:09 +0300

    feat: new test func and testing

--- annotation_test.go
+++ annotation_test.go
@@ -25,3 +25,45 @@ func TestSetConfig(t *testing.T) {
 		t.Error("SetConfig() failed")
 	}
 }
+func TestParseDirectory(t *testing.T) {
+	app := new(annotation.App)
+	app.SetConfig(annotation.Config{
+		Directory: "./example",
+		Output:    "routes.go",
+	})
+	app.ParseDirectory()
+	if len(app.Routes) == 0 {
+		t.Error("ParseDirectory() failed")
+	}
+}
+func TestParseAnnotationRoute(t *testing.T) {
+	app := new(annotation.App)
+	app.SetConfig(annotation.Config{
+		Directory: ".",
+		Output:    "routes.go",
+	})
+	annotation := `@Route("/foo", name="foo", methods={"GET"})`
+	route, err := app.ParseAnnotationRoute(annotation)
+	if err != nil {
+		t.Error("ParseAnnotationRoute() failed")
+	}
+	if route.Method != "GET" {
+		t.Error("ParseAnnotationRoute() failed")
+	}
+	if route.Path != "/foo" {
+		t.Error("ParseAnnotationRoute() failed")
+	}
+	if route.Name != "foo" {
+		t.Error("ParseAnnotationRoute() failed")
+	}
+}
+func TestGenerate(t *testing.T) {
+	app := new(annotation.App)
+	app.SetConfig(annotation.Config{
+		Directory: "./handler",
+		Output:    "routes.go",
+	})
+	app.ParseDirectory()
+	app.Generate()
+
+}
